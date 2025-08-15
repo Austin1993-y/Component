cf965f7f7fbf2e897936f61b09ba17670a96982a
Austin1993-y <austiny4466@gmail.com>
2025-08-15 15:40:42 +0530

    Update App.jsx
    unwanted code deleted

--- StopWatch/App.jsx
+++ StopWatch/App.jsx
@@ -1,13 +1,9 @@
-import StopWatch from "./assets/StopWatch";
 import Stopwatch from "./StopWatch";
-
-
 
 function App() {
   return (
     <div>
       <Stopwatch/>
-      <StopWatch/>
     </div>
   );
 }
